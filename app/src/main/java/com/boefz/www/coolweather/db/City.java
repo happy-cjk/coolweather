e60db1b038fa11b8b91735a7041abe07b3e231f6
CJK <chenjikun@boe.com.cn>
2018-08-13 21:00:24 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/boefz/www/coolweather/db/City.java
@@ -0,0 +1,50 @@
+package com.boefz.www.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by CJK on 2018/8/13.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
